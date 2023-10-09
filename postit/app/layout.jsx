a1a9dac65c61490ccc931f0eea58d5bef4c4b508
Kate Hooker <katehooker88@gmail.com>
2023-10-09 18:22:57 +0000

    updated next.config to uses google/github profile pic

--- postit/app/layout.jsx
+++ postit/app/layout.jsx
@@ -1,13 +1,13 @@
 import './globals.css'
-import Nav from './auth/Nav'
+import Nav from './Nav'
 //import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 
-const roboto = Roboto({ 
+const roboto = Roboto({
   subsets: ['latin'],
-  weight: ["400", "700"], 
-  variable: "--font-roboto",
- })
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+})
 
 //export const metadata: Metadata = {
 //  title: 'Create Next App',
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       {}
       <head />
-      <body className= {`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-gray-200`}>
+      <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-gray-200`}>
         <Nav />
         {children}
       </body>
--- postit/pages/auth/[...nextauth].js
+++ postit/pages/auth/[...nextauth].js
@@ -1,18 +1,19 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from "../../../prisma/client"
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '../../../prisma/client'
 
-//const adapter = PrismaAdapter(prisma)
+const adapter = PrismaAdapter(prisma)
 
-export default NextAuth({
-
+export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-  ]
-})
+  ],
+}
+
+export default NextAuth(authOptions)
